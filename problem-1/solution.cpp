7530a7ad0d4228d9216ec7894fe3edf262aa1bb8
Rohit Rathi <rathirohitg1997@gmail.com>
2020-03-02 19:56:19 +0530

    [Problem: 1] - Solution with binary search on real values

---
+++ problem-1/solution.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <cmath>
+#include <iomanip>
+
+#define long long ll;
+#define unsigned long long ull;
+
+using namespace std;
+
+int main() {
+	double number;
+	cin>>number;
+
+	bool isNegative = false;
+	if(number < 0) {
+		isNegative = true;
+		number = -number;
+	}
+
+	double answer;
+	if(number == 0) {
+		answer = 0;
+	} else if(number == 1) {
+		answer = 1;
+	} else {
+		double leftLimit = min(1.0, number);
+		double rightLimit = max(1.0, number);
+		double midValue = (leftLimit + rightLimit) / 2;
+
+		double newSquare;
+		do {
+			newSquare = midValue * midValue;
+
+			if(newSquare < number) {
+				leftLimit = midValue;
+			} else if(newSquare > number) {
+				rightLimit = midValue;
+			}
+
+			midValue = (leftLimit + rightLimit) / 2;
+			midValue = midValue;
+		} while(abs(newSquare - number) > 0.0001);
+
+		answer = midValue;
+	}
+
+	cout<<fixed<<setprecision(3);
+	if(isNegative) {
+		cout<<answer<<'i'<<endl;
+	} else {
+		cout<<answer<<endl;
+	}
+
+	return 0;
+}
