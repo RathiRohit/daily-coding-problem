71d2c4f8fc91e5a24c3dade4511405c88b9a33a3
Rohit Rathi <rathirohitg1997@gmail.com>
2020-12-22 17:34:02 +0530

    [Problem: 116] - Solution with use of maps and intersection finding approach

---
+++ problem-00116/solution.js
@@ -0,0 +1,66 @@
+import lineReader from 'line-reader';
+
+const allergensMap = {};
+const ingredientMap = {};
+const ingredientCount = {};
+
+function intersection(arrOfArr) {
+    const intersection = [];
+    const head = arrOfArr[0];
+    arrOfArr = [...arrOfArr].slice(1);
+    head.forEach(item => {
+        if (arrOfArr.every(items => items.includes(item))) {
+            intersection.push(item);
+        }
+    });
+    return intersection;
+}
+
+lineReader.eachLine('input.txt', (line, last) => {
+    const tokens = line.split(' ');
+    if (tokens[tokens.length - 1].includes('(')) {
+        const ingredients = tokens.slice(0, -1);
+        ingredients.forEach(ingredient => {
+            if (!ingredientCount[ingredient]) {
+                ingredientCount[ingredient] = 0;
+            }
+            ingredientCount[ingredient]++;
+        });
+
+        const allergens = tokens[tokens.length - 1].slice(1,-1).split(',');
+        allergens.forEach(allergen => {
+            if (!allergensMap[allergen]) {
+                allergensMap[allergen] = [];
+            }
+            allergensMap[allergen].push([...ingredients]);
+        });
+    }
+
+    if (last) {
+        while (Object.keys(allergensMap).length !== 0) {
+            Object.keys(allergensMap).forEach(allergen => {
+                const commonIngredients = intersection(allergensMap[allergen]);
+                const newCommonIngredients = [];
+
+                commonIngredients.forEach(ingredient => {
+                    if (!ingredientMap[ingredient]) {
+                        newCommonIngredients.push(ingredient);
+                    }
+                });
+
+                if (newCommonIngredients.length === 1) {
+                    ingredientMap[newCommonIngredients[0]] = allergen;
+                    delete allergensMap[allergen];
+                }
+            });
+        }
+
+        let ans = 0;
+        Object.keys(ingredientCount).forEach(ingredient => {
+            if (!ingredientMap[ingredient]) {
+                ans += ingredientCount[ingredient];
+            }
+        });
+        console.log(ans);
+    }
+});
